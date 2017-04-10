56eaadcd6f260f2a1976ce5fae38cd1e9d10398d
Keith Strickling <keithstrickling@gmail.com>
2017-04-10 17:32:04 -0400

    removed print

--- src/student_player/mytools/AlphaBetaMinimax.java
+++ src/student_player/mytools/AlphaBetaMinimax.java
@@ -190,7 +190,6 @@ public class AlphaBetaMinimax {
 		} else if (this.utilityFunction == 4 && this.numMovesMade != 1) {
 			return scoreAndBeanDifferenceWithBeansLeft2(boardState);
 		} else if (this.utilityFunction == 4 && this.numMovesMade == 1) {
-			System.out.println("here!");
 			return scoreAndBeanDifferenceWithBeansLeft3(boardState);
 		} else if (this.utilityFunction == 5) {
 			return scoreAndBeanDifferenceWithBeansLeft3(boardState);
