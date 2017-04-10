e231f7575e9fe526edccf718528b2570df6947e9
Keith Strickling <keithstrickling@gmail.com>
2017-04-10 13:23:59 -0400

    move change

--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -20,7 +20,7 @@ public class StudentPlayer extends BohnenspielPlayer {
 	private static final int MAX_MOVES = 11;
 
 	// the number of moves to take in the initial move
-	private static final int INITIAL_MOVES = 11;
+	private static final int INITIAL_MOVES = 10;
 	// number of moves to begin with (a good number determined experimentally)
 	private int numMovesToSimulate = 9;
 	// whether or not it is the first move
